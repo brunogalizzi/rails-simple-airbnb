a43d8dd49270815fec33f4a5b54bcec58c0fff5b
Bruno Toledo <brunogalizzi@gmail.com>
2020-04-28 22:51:43 -0300

    Upload files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create(name: 'Casa do Bertucci', description: 'Um ambiente aconchegante com a companhia de um doce cachorro chamado max!', address: 'Rua General Dionísio Cerqueira, 1152', price_per_night: 20, number_of_guests: 10, img_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200')
+Flat.create(name: 'Casa do Brunão', description: 'Venha curtir uma deliciosa suruba neste clássico lar de família!', address: 'Rua Ouro Preto, 1410', price_per_night: 200, number_of_guests: 8, img_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200')
+Flat.create(name: 'Dj Starling Cabaré', description: 'Com direito a show particular e uma partida de pôquer!', address: 'Edifício Felipe dos Santos', price_per_night: 110, number_of_guests: 5, img_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200')
